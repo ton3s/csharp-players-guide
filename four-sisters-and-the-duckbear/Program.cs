8ff0b7e4f705097375374143e386898db35879ee
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-08 18:19:33 -0500

    Add four sisters

---
+++ four-sisters-and-the-duckbear/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace four_sisters_and_the_duckbear;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("The Four Sisters and the Duckbear");
+        Console.WriteLine("How many eggs were gathered today?");
+        int eggsGathered = Convert.ToInt32(Console.ReadLine());
+
+        int numberOfSisters = 4;
+        int eggsPerSister = eggsGathered / numberOfSisters;
+        int eggsForDuckbear = eggsGathered % numberOfSisters;
+
+        Console.WriteLine("Eggs gathered: " + eggsGathered);
+        Console.WriteLine("Eggs per Sister: " + eggsPerSister);
+        Console.WriteLine("Eggs for Duckbear: " + eggsForDuckbear);
+    }
+}
---
+++ the-triangle-farmer/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace the_triangle_farmer;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("The Triangle Farmer");
+        Console.WriteLine("What is the triangle's base size?");
+        int baseSize = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("What is the triangle's height?");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        int area = baseSize * height / 2;
+        Console.WriteLine("The area of the triange with a base size of " + baseSize + " and a height of " + height + " is " + area);
+    }
+}
