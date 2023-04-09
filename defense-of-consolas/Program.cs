c171950d7e94d99027648abe1ec36a4c7a57dae2
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-08 20:47:23 -0500

    Add defense of consolas

---
+++ defense-of-consolas/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace defense_of_consolas;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Set console display
+        Console.Title = "The Defense of Consolas";
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+      
+        Console.Write("Target Row? ");
+        int row = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Target Column? ");
+        int col = Convert.ToInt32(Console.ReadLine());
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Deploy to:");
+
+        // Display Results
+        Console.Beep(); 
+
+        // Left
+        Console.WriteLine($"({row}, {col-1})");
+        // Top
+        Console.WriteLine($"({row+1}, {col})");
+        // Right
+        Console.WriteLine($"({row}, {col+1})");
+        // Bottom
+        Console.WriteLine($"({row-1}, {col})");
+    }
+}
