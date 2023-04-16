26b55d9753850bf9a161a6a6284f01eac32f6eef
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-16 15:41:59 -0500

    Complete simulas test

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -1,31 +1,13 @@
 ﻿namespace HelloWorld;
+
+enum Season { Winter, Spring, Summer, Fall }
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-
-        // Two of everything.
-        int i1, i2;
-        string s1, s2;
-        int[] a1, a2;
-
-         // Assign a value to the first.
-        i1 = 2;
-        s1 = "Hello";
-        a1 = new int[] { 1, 2, 4 };
-
-        // Copy to the second.
-        i2 = i1;
-        s2 = s1;
-        a2 = a1;
-
-         // Make changes.
-        i2 = 4;
-        a2[0] = -1;
-        s2 = "Bird";
-
-        Console.WriteLine(a1[0]);
-        Console.WriteLine(s1);
+        Season current = Season.Winter;
+        Console.WriteLine(current);
     }
 }
---
+++ simulas-test/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace simulas_test;
+
+enum ChestState { Open, Closed, Locked }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setup the console
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+
+        const string title = "Simula's Test";
+        Console.Title = title;
+        Console.WriteLine(title);
+
+        ChestState chestState = ChestState.Locked;
+
+        while (true) {
+            Console.Write($"This chest is {chestState}. What do you want to do? ");
+            string transition = Console.ReadLine();
+
+            switch (chestState) {
+                case ChestState.Locked: {
+                    if (transition == "unlock") chestState = ChestState.Closed;
+                    break;
+                }
+                case ChestState.Closed: {
+                    if (transition == "open") chestState = ChestState.Open;
+                    else if (transition == "lock") chestState = ChestState.Locked;
+                    break;
+                }
+                case ChestState.Open: {
+                    if (transition == "close") chestState = ChestState.Closed;
+                    break;
+                }
+            }
+        }
+    }
+}
