215eb48435768441d8894ecd58cdc5eea963a815
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-16 00:32:48 -0500

    Complete countdown

---
+++ countdown/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace countdown;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Countdown";
+
+        // Setup the console
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+
+        int number = AskForNumber("Enter a number to countdown from:");
+        Countdown(number);
+
+        void Countdown(int number)
+        {
+            Console.WriteLine(number);
+            if (number == 1) return;
+            Countdown(number - 1);
+        }
+
+        int AskForNumber(string text)
+        {
+            Console.Write($"{text} ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
