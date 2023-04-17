b8148eacf79bb114f5b832cd97fd84963ee32267
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-17 16:27:08 -0500

    Complete simulas soup

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -9,5 +9,17 @@ class Program
         Console.WriteLine("Hello, World!");
         Season current = Season.Winter;
         Console.WriteLine(current);
+
+        var score = GetScore(); 
+        Console.WriteLine(score.N);
+
+        DisplayScore(score);
+
+        (string N, int P, int L) GetScore() => ("R2-D2", 12420, 15);
+
+        void DisplayScore((string Name, int Points, int Level) score)
+        {
+            Console.WriteLine($"Name:{score.Name} Level:{score.Level} Score:{score.Points}");
+        }
     }
 }
---
+++ simulas-soup/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace simulas_soup;
+
+enum Type { Soup, Stew, Gumbo };
+enum Ingredient { Mushrooms, Chicken, Carrots, Potatoes };
+enum Seasoning { Spicy, Salty, Sweet };
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setup the console
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.WriteLine("Simula's Soup!");
+
+        // Prompt user
+        // Type
+        Type type;
+        string input;
+        do {
+            input = Ask("Type (Soup, Stew, Gumbo):"); 
+        } while (!Enum.TryParse<Type>(input, out type)); 
+
+        // Ingredient
+        Ingredient ingredient;
+        do {
+            input = Ask("Ingredient (Mushrooms, Chicken, Carrots, Potatoes):");
+        } while (!Enum.TryParse<Ingredient>(input, out ingredient)); 
+
+        // Seasoning
+        Seasoning seasoning;
+        do {
+            input = Ask("Seasoning (Spicy, Salty, Sweet):"); 
+        } while (!Enum.TryParse<Seasoning>(input, out seasoning)); 
+
+        // Initialize tuple
+        ( Type, Ingredient, Seasoning ) soup = (type, ingredient, seasoning);
+
+        Console.WriteLine($"{soup.Item3} {soup.Item2} {soup.Item1}");
+
+        string Ask(string text)
+        {
+            Console.Write($"{text} ");
+            return Console.ReadLine();
+        }
+    }
+}
