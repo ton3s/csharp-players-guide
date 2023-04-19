ed328624325018c6f128816bca73ac68ae5cd274
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-18 23:50:14 -0500

    Complete the cards

--- entering-the-catacombs/Program.cs
+++ entering-the-catacombs/Program.cs
@@ -18,11 +18,12 @@ class Program
         displayPoint(p2);
         displayPoint(p3);
 
-        void displayPoint(Point p) {
+        void displayPoint(Point p)
+        {
             Console.WriteLine($"({p.X}, {p.Y})");
         }
 
-        // Color
+        // Colors
         Console.ForegroundColor = ConsoleColor.Magenta;
         Color customColor = new Color { R = 128, G = 255, B = 0 };
         Color green = Color.Green();
@@ -30,8 +31,41 @@ class Program
         displayColor(customColor);
         displayColor(green);
 
-        void displayColor(Color c) {
+        void displayColor(Color c)
+        {
             Console.WriteLine($"({c.R}, {c.G}, {c.B})");
+        }
+
+        // Cards
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        string[] cardRanks = Enum.GetNames(typeof(CardRank));
+        string[] cardColors = Enum.GetNames(typeof(CardColor));
+        Card[] deck = new Card[cardColors.Length * cardRanks.Length];
+        int cardCount = 0;
+
+        // Create each card and add to the desk
+        for (int i = 0; i < cardColors.Length; i++)
+        {
+            for (int j = 0; j < cardRanks.Length; j++)
+            {
+                // Convert string to enum
+                CardColor cardColor = (CardColor)Enum.Parse(typeof(CardColor), cardColors[i], true);
+                CardRank cardRank = (CardRank)Enum.Parse(typeof(CardRank), cardRanks[j], true);
+                Card card = new Card(cardColor, cardRank);
+                deck[cardCount] = card;
+                cardCount++;
+            }
+        }
+
+        // Display all the cards in the deck
+        displayCards(deck);
+       
+        void displayCards(Card[] deck)
+        {
+            foreach (Card card in deck)
+            {
+                Console.WriteLine($"The {card.Color} {card.Rank}");
+            }
         }
     }
 
@@ -70,4 +104,31 @@ class Program
         public static Color Blue() => new Color(0, 0, 255);
         public static Color Purple() => new Color(128, 0, 128);
     }
+
+    public class Card
+    {
+        public CardColor Color { get; init; }
+        public CardRank Rank { get; init; }
+        public bool IsSymbol { get; } = false;
+
+        public Card(CardColor color, CardRank rank)
+        {
+            Color = color;
+            Rank = rank;
+
+            switch (rank) {
+                case CardRank.Dollar:
+                case CardRank.Percent:
+                case CardRank.Carat:
+                case CardRank.Ambersand:
+                    IsSymbol = true;
+                    break;
+            }
+        }
+
+
+    }
+
+    public enum CardColor { Red, Green, Blue, Yellow };
+    public enum CardRank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Dollar, Percent, Carat, Ambersand };
 }
