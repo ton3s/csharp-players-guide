7a9b7824657436c0ce24da5530143581ef786105
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-18 21:51:49 -0500

    Complete the point

---
+++ entering-the-catacombs/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace entering_the_catacombs;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setup the console
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Entering the Catacombs");
+        
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(-4, 0);
+        Point p3 = new Point();
+
+        displayPoint(p1);
+        displayPoint(p2);
+        displayPoint(p3);
+
+        void displayPoint(Point p) {
+            Console.WriteLine($"({p.X}, {p.Y})");
+        }
+    }
+
+    // Classes
+    public class Point {
+        public float X { get; }
+        public float Y { get; }
+
+        public Point() : this(0, 0) {}
+
+        public Point(float x, float y) {
+            X = x;
+            Y = y;
+        }
+    }
+}
