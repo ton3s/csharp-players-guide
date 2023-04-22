0b346cccf665cfe91b41fc6529cd07fd8bee18e2
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-22 01:50:56 -0500

    Complete robotic interface

---
+++ robotic-interface/Program.cs
@@ -0,0 +1,95 @@
+﻿// Setup the console
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Robotic Interface");
+
+Robot robot = new Robot();
+
+// Prompt user for 3 commands
+for (int i = 0; i < robot.Commands.Length; i++) {
+    robot.Commands[i] = GetRobotCommand(Console.ReadLine());
+}
+robot.RunProgram();
+
+RobotCommand GetRobotCommand(string command) {
+    return command switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SouthCommand(),
+        "east" => new EastCommand(),
+        "west" => new WestCommand()
+    };
+}
+
+// Classes
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public RobotCommand?[] Commands { get; } = new RobotCommand?[3];
+
+    public void RunProgram()
+    {
+        foreach (RobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public interface RobotCommand
+{
+    public abstract void Run(Robot robot);
+}
+
+public class OnCommand: RobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : RobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class NorthCommand : RobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y++;
+    }
+}
+
+public class SouthCommand : RobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y--;
+    }
+}
+
+public class WestCommand : RobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X--;
+    }
+}
+
+public class EastCommand : RobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X++;
+    }
+}
