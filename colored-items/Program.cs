cf747854a9c465766b8792cdb3102118cdd66d79
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-23 15:56:22 -0500

    Complete colored items

---
+++ colored-items/Program.cs
@@ -0,0 +1,30 @@
+﻿// Setup the console
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Colored Items");
+
+ColoredItem<Sword> sword = new ColoredItem<Sword> { Color = ConsoleColor.Blue };
+sword.Display();
+
+ColoredItem<Bow> bow = new ColoredItem<Bow> { Color = ConsoleColor.Red };
+bow.Display();
+
+ColoredItem<Axe> axe = new ColoredItem<Axe> { Color = ConsoleColor.Green };
+axe.Display();
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
+
+public class ColoredItem<T> where T: new()
+{
+    public T Item { get; } = new T();
+    public ConsoleColor Color { get; init; }
+
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item);
+    }
+
+}
