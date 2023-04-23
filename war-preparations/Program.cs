32e3a46af8be2a1542374e32efe1495332e65ee3
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-23 14:16:31 -0500

    Complete war preparations

---
+++ war-preparations/Program.cs
@@ -0,0 +1,17 @@
+﻿// Setup the console
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("War Preparations");
+
+Sword s1 = new Sword(Material.Iron, Gem.None, 50f, 10f);
+Sword s2 = s1 with { Material = Material.Bronze, Width = 10f };
+Sword s3 = s1 with { Material = Material.Binarium, Gem = Gem.Bitstone, Length = 90f };
+
+Console.WriteLine(s1.ToString());
+Console.WriteLine(s2.ToString());
+Console.WriteLine(s3.ToString());
+
+public record Sword(Material Material, Gem Gem, float Length, float Width);
+
+public enum Material { Wood, Bronze, Iron, Steel, Binarium }
+public enum Gem { Emerald, Amber, Sapphire, Diamond, Bitstone, None }
