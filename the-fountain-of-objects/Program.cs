39fceae9f7a513b8ba5f55dcc0364fc8fbec14a2
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-23 19:58:39 -0500

    Complete the fountain of objects

--- the-fountain-of-objects/Program.cs
+++ the-fountain-of-objects/Program.cs
@@ -1,6 +1,6 @@
 ﻿// The Fountain of Objects
 Console.Clear();
-Console.ForegroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
 Console.WriteLine("The Fountain of Objects");
 
 GameManager gameManager = new GameManager();
@@ -10,6 +10,7 @@ public class GameManager
 {
     private RoomsManager RoomsManager { get; }
     private Player Player { get; }
+    private bool _isFountainActivated = false;
 
     public GameManager()
     {
@@ -19,10 +20,80 @@ public class GameManager
 
     public void StartGame()
     {
-        RoomsManager.GetStatus();
+        do
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Console.WriteLine($"You are in the room at (Row={Player.Position.Row}, Column={Player.Position.Column})");
+            if (IsGameOver()) break;
+            RoomsManager.DisplayRoomStatus(Player.Position);
+            string action = Player.GetAction();
+            ProcessAction(action);
+        } while (true);
+    }
+
+    private bool IsGameOver()
+    {
+        if (_isFountainActivated && Player.Position.Row == 0 && Player.Position.Column == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("The Fountain of Objects has been reactivated, and you have escaped with your life!\nYou Win!");
+            return true;
+        }
+        return false;
+    }
+
+    private void ProcessAction(string action)
+    {
+        // Process movement
+        switch (action)
+        {
+            case "move north":
+                if (Player.Position.Row == 0) DisplayEndOfMapErrorMessage();
+                else Player.Position = Player.Position with { Row = Player.Position.Row - 1 };
+                break;
+
+            case "move south":
+                if (Player.Position.Row == 3) DisplayEndOfMapErrorMessage();
+                else Player.Position = Player.Position with { Row = Player.Position.Row + 1 };
+                break;
+
+            case "move east":
+                if (Player.Position.Column == 3) DisplayEndOfMapErrorMessage();
+                else Player.Position = Player.Position with { Column = Player.Position.Column + 1 };
+                break;
+
+            case "move west":
+                if (Player.Position.Column == 0) DisplayEndOfMapErrorMessage();
+                else Player.Position = Player.Position with { Column = Player.Position.Column - 1 };
+                break;
+
+            case "enable fountain":
+                // Check if we are in the fountain room
+                Room room = RoomsManager.GetRoom(Player.Position);
+                if (room.GetType().ToString() == "Fountain")
+                {
+                    ((Fountain)room).IsFountainActivated = true;
+                    _isFountainActivated = true;
+                }
+                else
+                {
+                    DisplayErrorMessage("Sorry. You can only perform this action in the fountain room.");
+                }
+                break;
+        }
+
+        void DisplayErrorMessage(string error)
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
+            Console.ForegroundColor = prevColor;
+        }
+
+        void DisplayEndOfMapErrorMessage() => DisplayErrorMessage("Sorry. You cannot moved in this direction!");
     }
 }
-
 
 public class RoomsManager
 {
@@ -36,21 +107,32 @@ public class RoomsManager
         {
             for (int column = 0; column < Rooms.GetLength(1); column++)
             {
-                Rooms[row, column] = new Room(new Position(row, column));
+                if (row == 0 && column == 0)
+                {
+                    // Entrance
+                    Rooms[row, column] = new Entrance(new Position(row, column));
+                }
+                else if (row == 0 && column == 2)
+                {
+                    // Fountain
+                    Rooms[row, column] = new Fountain(new Position(row, column));
+                }
+                else
+                {
+                    // Empty Room
+                    Rooms[row, column] = new Room(new Position(row, column));
+                }
             }
         }
     }
 
-    public void GetStatus()
+    public void DisplayRoomStatus(Position position)
     {
-        for (int row = 0; row < Rooms.GetLength(0); row++)
-        {
-            for (int column = 0; column < Rooms.GetLength(1); column++)
-            {
-                Console.WriteLine(Rooms[row, column].GetDescription());
-            }
-        }
+        string? description = Rooms[position.Row, position.Column].GetDescription();
+        if (description != null) Console.WriteLine(description);
     }
+
+    public Room GetRoom(Position position) => Rooms[position.Row, position.Column];
 }
 
 public class Room
@@ -62,9 +144,35 @@ public class Room
         Position = position;
     }
 
-    public string? GetDescription()
+    public virtual string? GetDescription() => null;
+}
+
+public class Entrance : Room
+{
+    public Entrance(Position position) : base(position) { }
+
+    public override string GetDescription()
     {
-        return Position.ToString();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        return "You see light coming from the cavern entrance.";
+    }
+}
+
+public class Fountain : Room
+{
+    public bool IsFountainActivated { get; set; }
+
+    public Fountain(Position position) : base(position)
+    {
+        IsFountainActivated = false;
+    }
+
+    public override string GetDescription()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        return IsFountainActivated
+            ? "You hear the rushing waters from the Fountain of Objects. It has been reactivated!"
+            : "You hear water dripping in this room. The Fountain of Objects is here!";
     }
 }
 
@@ -79,6 +187,7 @@ public class Player
 
     public string GetAction()
     {
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.Write("What do you want to do? ");
         return Console.ReadLine();
     }
