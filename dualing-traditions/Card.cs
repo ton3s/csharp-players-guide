b0e4746c316eea9534f381cb4967697a2702ff8c
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-28 16:14:50 -0500

    Complete dualing traditions

---
+++ dualing-traditions/Card.cs
@@ -0,0 +1,26 @@
+﻿namespace the_card;
+
+partial class Program
+{
+    public class Card
+    {
+        public CardColor Color { get; init; }
+        public CardRank Rank { get; init; }
+        public bool IsSymbol { get; } = false;
+
+        public Card(CardColor color, CardRank rank)
+        {
+            Color = color;
+            Rank = rank;
+
+            switch (rank) {
+                case CardRank.Dollar:
+                case CardRank.Percent:
+                case CardRank.Carat:
+                case CardRank.Ambersand:
+                    IsSymbol = true;
+                    break;
+            }
+        }
+    }
+}
