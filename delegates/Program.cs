bac78995bca932f1c84b979ba8bdb78dc7859c87
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-30 16:18:59 -0500

    Complete the sieve

---
+++ delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Delegates");
+
+var results = ChangeArrayElements(new int[] { 1, 2, 3, 4, 5 }, MultiplyTwo);
+
+foreach (int result in results)
+{
+    Console.WriteLine(result);
+}
+
+
+// Delegate methods
+int AddOne(int number) => number + 1;
+int MultiplyTwo(int number) => number * 2;
+
+int[] ChangeArrayElements(int[] numbers, Func<int, int> operation)
+{
+    int[] result = new int[numbers.Length];
+    for (int index = 0; index < result.Length; index++)
+        result[index] =  operation.Invoke(numbers[index]);
+    return result;
+}
---
+++ the-sieve/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("The Sieve");
+
+bool IsEven(int number) => number % 2 == 0;
+bool IsPositive(int number) => number > 0;
+bool IsMultipleOfTen(int number) => number % 10 == 0;
+
+Console.WriteLine("Please select a filter: ");
+Console.WriteLine("1 - Is Even");
+Console.WriteLine("2 - Is Positive");
+Console.WriteLine("3 - Is Multiplier of Ten");
+string? input = Console.ReadLine();
+
+Sieve sieve = input switch
+{
+    "1" => new Sieve(IsEven),
+    "2" => new Sieve(IsPositive),
+    "3" => new Sieve(IsMultipleOfTen)
+};
+
+// Ask user to test the delegate
+while (true)
+{
+    int numberToTest = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{sieve.IsGood(numberToTest)}");
+}
+
+public class Sieve
+{
+    private Predicate<int> _operation;
+
+    public Sieve(Predicate<int> operation)
+    {
+        _operation = operation;
+    }
+
+    public bool IsGood(int number) => _operation.Invoke(number);
+}
