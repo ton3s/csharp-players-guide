d9c02b0ce43e7f90944088955a1cf3b2a4000e6a
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-30 17:27:45 -0500

    Complete charberry trees

---
+++ charberry-trees/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Charberry Trees");
+
+CharberryTree tree = new CharberryTree();
+new Notifier(tree);
+new Harvester(tree);
+
+while (true)
+    tree.MaybeGrow();
+
+public class Notifier
+{
+    public CharberryTree Tree { get; }
+    private int _count = 0;
+
+    public Notifier(CharberryTree tree)
+    {
+        Tree = tree;
+        Tree.Ripened += OnRipened;
+    }
+
+    public void OnRipened()
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine($"[Notifier] A charberry fruit has ripened! [{_count}]");
+        _count++;
+    }
+}
+
+public class Harvester
+{
+    public CharberryTree Tree { get; }
+    private int _count = 0;
+
+    public Harvester(CharberryTree tree)
+    {
+        Tree = tree;
+        Tree.Ripened += OnRipened;
+    }
+
+    public void OnRipened()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"[Harvester] Harvesting charberry fruit! [{_count}]");
+        Tree.Ripe = false;
+        _count++;
+    }
+}
+
+public class CharberryTree
+{
+    private Random _random = new Random();
+    public bool Ripe { get; set; }
+    public event Action? Ripened;
+
+    public void MaybeGrow()
+    {
+        // Only a tiny chance of ripening each time, but we try a lot!
+        if (_random.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            Ripened?.Invoke();
+        }
+    }
+}
+
