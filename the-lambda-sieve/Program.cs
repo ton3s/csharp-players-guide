afbc7d6c32d62d1de85d9ac668d9ff6a1eba371b
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-04-30 18:07:50 -0500

    Complete the lambda sieve

---
+++ the-lambda-sieve/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("The Lambda Sieve");
+
+Console.WriteLine("Please select a filter: ");
+Console.WriteLine("1 - Is Even");
+Console.WriteLine("2 - Is Positive");
+Console.WriteLine("3 - Is Multiplier of Ten");
+string? input = Console.ReadLine();
+
+Sieve sieve = input switch
+{
+    "1" => new Sieve(n => n % 2 == 0),
+    "2" => new Sieve(n => n > 0),
+    "3" => new Sieve(n => n % 10 == 0)
+};
+
+// Ask user to test the delegate
+while (true)
+{
+    int numberToTest = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{sieve.IsGood(numberToTest)}");
+}
+
+public class Sieve
+{
+    private Predicate<int> _operation;
+
+    public Sieve(Predicate<int> operation)
+    {
+        _operation = operation;
+    }
+
+    public bool IsGood(int number) => _operation.Invoke(number);
+}
