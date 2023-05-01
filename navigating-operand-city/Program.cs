4a013a13bc3f131bc9f90372e03446f3dc644c91
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-05-01 15:40:21 -0500

    Add navigating operand city

---
+++ navigating-operand-city/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Navigating Operand City");
+
+// Test operators
+BlockCoordinate bc = new BlockCoordinate(4, 3);
+
+// Add block offset
+BlockOffset bo = new BlockOffset(2, 0);
+Console.WriteLine(bo + bc);
+Console.WriteLine(bc + bo);
+
+// Add direction
+Direction direction = Direction.East;
+Console.WriteLine(bc + direction);
+Console.WriteLine(direction + bc);
+
+public record BlockCoordinate(int Row, int Column)
+{
+    // BlockCoordinate + BlockOffset
+    public static BlockCoordinate operator +(BlockCoordinate bc, BlockOffset bo)
+        => new BlockCoordinate(bc.Row + bo.RowOffset, bc.Column + bo.ColumnOffset);
+    public static BlockCoordinate operator +(BlockOffset bo, BlockCoordinate bc)
+        => new BlockCoordinate(bc.Row + bo.RowOffset, bc.Column + bo.ColumnOffset);
+
+    // BlockCoordinate + Direction
+    public static BlockCoordinate operator +(BlockCoordinate bc, Direction direction)
+    {
+        return direction switch
+        {
+            Direction.North => bc with { Row = bc.Row - 1 },
+            Direction.South => bc with { Row = bc.Row + 1 },
+            Direction.East => bc with { Column = bc.Column + 1},
+            Direction.West => bc with { Column = bc.Column - 1 }
+        };       
+    }
+    public static BlockCoordinate operator +(Direction direction, BlockCoordinate bc)
+    {
+        return direction switch
+        {
+            Direction.North => bc with { Row = bc.Row - 1 },
+            Direction.South => bc with { Row = bc.Row + 1 },
+            Direction.East => bc with { Column = bc.Column + 1 },
+            Direction.West => bc with { Column = bc.Column - 1 }
+        };
+    }
+
+}
+public record BlockOffset(int RowOffset, int ColumnOffset);
+public enum Direction { North, South, East, West }
--- operator-overloading/Program.cs
+++ operator-overloading/Program.cs
@@ -2,5 +2,24 @@
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("operator Overloading");
 
+Point a = new Point(2, 3);
+Point b = new Point(1, 8);
 
-public record Point(double X, double Y);
+// Addition
+Console.WriteLine(a + b);
+
+// Multiplication
+Console.WriteLine(a * 2);
+
+public record Point(double X, double Y)
+{
+    // Add points together
+    public static Point operator +(Point a, Point b)
+        => new Point(a.X + b.X, a.Y + b.Y);
+
+    // Scalar multiplication
+    public static Point operator *(Point p, double scalar)
+        => new Point(p.X * scalar, p.Y * scalar);
+    public static Point operator *(double scalar, Point p)
+        => new Point(p.X * scalar, p.Y * scalar);
+}
