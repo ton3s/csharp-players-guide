826cd0b3a7c9d4dd4059a69a1b7c43b80110ab51
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-05-01 16:08:19 -0500

    Complete converting directions to offsets

---
+++ converting-directions-to-offsets/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Converting Directions to Offsets");
+
+BlockOffset b = Direction.South;
+Console.WriteLine(b);
+
+public record BlockOffset(int RowOffset, int ColumnOffset)
+{
+    // Convert direction => block offset
+    public static implicit operator BlockOffset(Direction d)
+        => d switch
+        {
+            Direction.North => new BlockOffset(-1, 0),
+            Direction.South => new BlockOffset(1, 0),
+            Direction.East => new BlockOffset(0, 1),
+            Direction.West => new BlockOffset(0, -1)
+        };
+}
+public enum Direction { North, South, East, West }
