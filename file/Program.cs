50952af5ec32a4f7b388c3d6e2b3d54b0d047c8e
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-05-01 00:24:38 -0500

    Complete the long game

---
+++ file/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("The File Class");
+
+// Check if the file exists to read the message from last time
+if (File.Exists("message.txt"))
+{
+    string previous = File.ReadAllText("message.txt");
+    Console.WriteLine($"Last time the message was: {previous}");
+}
+
+Console.Write("What do you want to tell me next time? ");
+string? message = Console.ReadLine();
+
+File.WriteAllText("message.txt", message);
---
+++ the-long-game/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("The Long Game");
+
+// Ask for the username
+Console.Write("What is your username? ");
+string? username = Console.ReadLine();
+
+// Check if previous score exists
+int score = 0;
+if (File.Exists($"{username}.txt"))
+{
+    score = Convert.ToInt32(File.ReadAllText($"{username}.txt"));
+}
+
+// Incremet score for each key press
+while (!(Console.ReadKey().Key == ConsoleKey.Enter)) {
+    score++;
+    Console.WriteLine($" - Keys Pressed: {score}");
+}
+
+// Write the score in the file
+File.WriteAllText($"{username}.txt", score.ToString());
